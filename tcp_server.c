f7f81b45431998103e8d776e30404dc20f9dfcac
Mid666 <64549631+Mid666@users.noreply.github.com>
2021-01-07 13:21:13 +0530

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -1,13 +1,16 @@
 /*
-	Midhun Nair
-	Updated Date: 6/1/2021 
-	TCP-SERVER
+    MIDHUN NAIR
+   
+    Date:   07/01/2021
+
+    TCP-SERVER IMPLEMENTATION
 
 */
 
-#include<sys/types.h>     //socket, bind, listen, accept API s
+#include<stdio.h>
+#include<sys/types.h>     
 #include<sys/socket.h>
-#include<unistd.h>        //read, write
+#include<unistd.h>      
 #include<netinet/in.h>
 #include<netinet/ip.h>
 #include<string.h>
@@ -19,105 +22,94 @@ void SIGINT_HANDLER(int);
 char sendbuffer[1024];
 char receivebuffer[1024];
 
-int sockfd;    // socket descriptor to listen incoming connection request.
-int connfd;    // socket descriptor to establish connection with client, for accept system call to be used for sending responses. 
+int sockfd;   
+int connfd;   
 
 int main(int argc, char *argv[])
 {
-  int portnumber, client_addlen;
-  int readbytes;
+    int portnumber, client_addlen;
+    int readbytes;
   
-  if(argc < 2)
-  {
-      printf("Error: Pass the Server port no. as first argument above 1023\n");
-      exit(1);
-  }
-  
-  /*   struct sockaddr_in
-   *   {
-   *      sa_family sin_family; // family(ipv4/ipv6) [AF_INET:ipv4]     
-   *      in_port_t sin_port;   // port in network byte order
-   *      struct in_addr sin_addr; //only ip address
-   *   };
-   *    
-   *    struct in_addr 
-	 *    {
-   *       uint32_t       s_addr;    // address in network byte order
-   *    };
-   */
+    if(argc < 2)
+    {
+        printf("Error: Pass the Server port no. as first argument above 1023\n");
+        exit(EXIT_FAILURE);
+    }
    
-   struct sockaddr_in server_addr;
-   struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
    
-   bzero((char *)&server_addr, sizeof(server_addr));
-   bzero((char *)&client_addr, sizeof(client_addr));
+    bzero((char *)&server_addr, sizeof(server_addr));
+    bzero((char *)&client_addr, sizeof(client_addr));
    
-   portnumber = atoi(argv[1]);   
+    portnumber = atoi(argv[1]);   /* converting string to int */
    
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);  // SOCK_STREAM: TCP
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);  
    
-   if(sockd < 0)
-   {
-      perror("Err: Failed to create socket\n");
-      exit(2);
-   }
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = INADDR_ANY;  //any address
-   server_addr.sin_port = htons(portnumber);   //add and port are stored in network byte order, hence htons
-   
-    /* When a socket is created it assumes the IP address of the network node that created it. 
-	  If a socket has an IP address but not a port number it is said to be ‘unbound’. 
-	  An unbound socket cannot receive data because it does not have a complete address */
+    if(sockfd < 0)
+    {
+        perror("Err: Failed to create socket\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;  
+    server_addr.sin_port = htons(portnumber);  
     
     int ret = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if(ret < 0)
     {
-      perror("Err: Failed to bind the socket. UNBOUND SOCKET.\n");
-      exit(3);
+        perror("Err: Failed to bind the socket. UNBOUND SOCKET.\n");
+        exit(EXIT_FAILURE);
     }
-    
-    int pending_requests = 5; //MAX 5 pending requests in queue
-    int retval = listen(sockd, pending_requests); //listen to max 5 requests through sockfd.
+    else
+        printf("Binding successful\n");    
+
+    int pending_requests = 5;
+    int retval = listen(sockfd, pending_requests);
     if(retval < 0)
     {
-      perror("Err:Failed to accept request\n");
-      exit(4);
+        perror("Err:Failed to accept request\n");
+        exit(EXIT_FAILURE);
     }
-    
+    else 
+        printf("Listening....\n");
+
     client_addlen = sizeof(client_addr);
-    
-    /* To establish connection with client: To accept client */
+
     connfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addlen);
     if(connfd < 0)
     {
-      perror("Error in fetching connection request\n");	
-      exit(5);
+        perror("Error in fetching connection request\n");	
+        exit(EXIT_FAILURE);
     }
-    
+    else
+        printf("Connected to Client\n\n");
+
     /*************SENDER-RECEIVER*********************/
     
     pid_t retf = fork();
     if(retf < 0)
     {
-      perror("");
-      exit(6);
+        perror("");
+        exit(EXIT_FAILURE);
     } 
     
     /****SENDING TO CLIENT****/
     
     if(retf > 0)
     {
-      while(1)
-      {
-        signal(SIGINT, SIGINT_HANDLER);
-        bzero(&sendbuffer, sizeof(sendbuffer));
-        fgets(sendbufffer, sizeof(sendbuffer), stdin);
-        size_t sret = send(connfd, sendbuffer, strlen(sendbuffer)+1, 0);
-        if(sret < 0)
+        while(1)
         {
-          printf("\nsending failed..Try Again\n");
+            signal(SIGINT, SIGINT_HANDLER);
+            bzero(&sendbuffer, sizeof(sendbuffer));
+            fgets(sendbuffer, sizeof(sendbuffer), stdin);
+            size_t sret = send(connfd, sendbuffer, strlen(sendbuffer)+1, 0);
+            if(sret < 0)
+            {
+                printf("\nsending failed..Try Again\n");
+            }
         }
-      }
     }
    
     /****RECEIVING FROM CLIENT****/
@@ -126,32 +118,31 @@ int main(int argc, char *argv[])
     {
         while(1)
         {
-          bzero(&receivebuffer, sizeof(receivebuffer));
-          size_t rret = recv(connf, receivebuffer, sizeof(receivebuffer), 0);
-          if(!strcmp(receivebuffer, "Disconnect\n")
-          {
-            printf("Diconnecting..\nDisconnected\n");
-            exit(0);
-          }
-          printf("Client: %s\n", receivebuffer);
+            signal(SIGINT, SIGINT_HANDLER);
+            bzero(&receivebuffer, sizeof(receivebuffer));
+            size_t rret = recv(connfd, receivebuffer, sizeof(receivebuffer), 0);
+            if(!strcmp(receivebuffer, "Disconnect\n"))
+            {
+                printf("Diconnecting..\nDisconnected\n");
+                exit(0);
+            }
+            printf("Msg from Client: %s\n", receivebuffer);
         }
-    }
-    
-  close(connfd);
-  close(sockfd);
-  return 0;  
+    } 
+    close(connfd);
+    close(sockfd);
+    return 0;  
 }    
+
+/* SIGNAL HANDLER IF USER PRESS CTRL+C */
 
 void SIGINT_HANDLER(int signum)
 {
-  strcpy(sendbuffer,"\nServer Disconnected\n");
-  send(connfd, sendbuffer, strlen(sendbuffer)+1, 0);
-  printf("\nDisconnected Server\n");
-  close(connfd);
-  close(sockfd);
-  exit(2);
+    strcpy(sendbuffer,"\nServer Disconnected\n");
+    send(connfd, sendbuffer, strlen(sendbuffer)+1, 0);
+    printf("\nDisconnected Server\n");
+    close(connfd);
+    close(sockfd);
+    exit(EXIT_FAILURE);
 }
     
-   
-
-
